488091b774643a3f5ffe13f2969cbcacd6263179
Scott McCauley <scottmccauley@SuzySmall.local>
2019-05-08 13:37:48 -0400

    prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,21 +7,21 @@ module.exports = {
   parserOptions: {
     parser: 'babel-eslint'
   },
-  extends: [
-    '@nuxtjs',
-    'plugin:prettier/recommended'
-  ],
-  plugins: [
-    'prettier'
-  ],
+  extends: ['@nuxtjs', 'plugin:prettier/recommended'],
+  plugins: ['prettier'],
   // add your custom rules here
   rules: {
     // Only allow debugger in development
     'no-debugger': process.env.PRE_COMMIT ? 'error' : 'off',
     // Only allow `console.log` in development
-    'no-console': process.env.PRE_COMMIT ? ['error', {
-      allow: ['warn', 'error']
-    }] : 'off',
+    'no-console': process.env.PRE_COMMIT
+      ? [
+          'error',
+          {
+            allow: ['warn', 'error']
+          }
+        ]
+      : 'off',
     'vue/array-bracket-spacing': 'error',
     'vue/arrow-spacing': 'error',
     'vue/block-spacing': 'error',
