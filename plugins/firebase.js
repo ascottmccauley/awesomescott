187cceaa71df9bde904c7e0c65615a2ea5981b00
Scott McCauley <scottmccauley@SuzySmall.local>
2019-05-09 08:54:44 -0400

    last try...

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -6,12 +6,20 @@ import 'firebase/firestore'
 
 if (!firebase.apps.length) {
   const config = {
-    apiKey: process.env.apiKey,
-    authDomain: process.env.authDomain,
-    databaseURL: process.env.databaseURL,
-    projectId: process.env.projectId,
-    storageBucket: process.env.storageBucket,
-    messagingSenderId: process.env.messagingSenderId
+    // apiKey: process.env.apiKey,
+    // authDomain: process.env.authDomain,
+    // databaseURL: process.env.databaseURL,
+    // projectId: process.env.projectId,
+    // storageBucket: process.env.storageBucket,
+    // messagingSenderId: process.env.messagingSenderId,
+    // appId: process.env.appId
+    apiKey: 'AIzaSyBDvTbaYIwtWmc8nEIsTLPnunT3HEGQHFg',
+    authDomain: 'awesomescott.firebaseapp.com',
+    databaseURL: 'https://awesomescott.firebaseio.com',
+    projectId: 'awesomescott',
+    storageBucket: 'awesomescott.appspot.com',
+    messagingSenderId: '399213857457',
+    appId: '1:399213857457:web:2c4f52f92a39ef20'
   }
   firebase.initializeApp(config)
 }
