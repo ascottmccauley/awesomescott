fad8f4001f770239a4f647d818ead19ab50a6a93
Scott McCauley <scottmccauley@SuzySmall.local>
2019-05-09 17:51:34 -0400

    yep...

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -2,27 +2,19 @@ import * as firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/firestore'
 
-// require('dotenv').config()
-
 if (!firebase.apps.length) {
   const config = {
-    apiKey: process.env.apiKey,
-    authDomain: process.env.authDomain,
-    databaseURL: process.env.databaseURL,
-    projectId: process.env.projectId,
-    storageBucket: process.env.storageBucket,
-    messagingSenderId: process.env.messagingSenderId,
-    appId: process.env.appId
-    // apiKey: 'AIzaSyBDvTbaYIwtWmc8nEIsTLPnunT3HEGQHFg',
-    // authDomain: 'awesomescott.firebaseapp.com',
-    // databaseURL: 'https://awesomescott.firebaseio.com',
-    // projectId: 'awesomescott',
-    // storageBucket: 'awesomescott.appspot.com',
-    // messagingSenderId: '399213857457',
-    // appId: '1:399213857457:web:2c4f52f92a39ef20'
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
   }
   firebase.initializeApp(config)
 }
+// TODO: add settings and rules to firestore
 // firebase.firestore().settings()
 const fireDb = firebase.firestore()
 const auth = firebase.auth()
